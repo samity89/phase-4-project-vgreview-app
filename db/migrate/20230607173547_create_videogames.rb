ca170c2cda284e9afd33003279a805c5d25806cb
Samuel Amity <sam.amity@gmail.com>
2023-06-07 13:37:04 -0400

    first commit

---
+++ db/migrate/20230607173547_create_videogames.rb
@@ -0,0 +1,14 @@
+class CreateVideogames < ActiveRecord::Migration[7.0]
+  def change
+    create_table :videogames do |t|
+      t.string :name
+      t.string :developer
+      t.date :release_date
+      t.string :genre
+      t.string :image_url
+      t.string :platform
+
+      t.timestamps
+    end
+  end
+end
