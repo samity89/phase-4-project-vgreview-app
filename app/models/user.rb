b6807ce994df982bcd7b3cef85b5ec5ef36e05d5
Samuel Amity <sam.amity@gmail.com>
2023-06-07 13:52:03 -0400

    bcrypt support added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,16 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reviews, dependent: :destroy
     has_many :videogames, through: reviews
+    
+    def password=(new_password)
+        salt = BCrypt::Engine::generate_salt
+        self.password_digest = BCrypt::Engine::hash_secret(new_password, salt)
+    end
+
+    def authenticate(password)
+        salt = password_digest[0..28]
+        return nil unless BCrypt::Engine::hash_secret(password, salt) == self.password_digest
+        self
+    end
+
 end
