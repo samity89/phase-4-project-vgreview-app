1775c667c34ed4c29a67bc968bf583898190dc70
Samuel Amity <sam.amity@gmail.com>
2023-06-08 15:31:33 -0400

    user validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     has_many :reviews, dependent: :destroy
     has_many :videogames, through: :reviews
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
     
     def password=(new_password)
         salt = BCrypt::Engine::generate_salt
