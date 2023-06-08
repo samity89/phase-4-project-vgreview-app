59925afa53357808d8f4485ed3b79e93f8c11478
Samuel Amity <sam.amity@gmail.com>
2023-06-08 15:45:53 -0400

    user validation correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_many :videogames, through: :reviews
     validates :username, presence: true, uniqueness: true
     validates :password, presence: true
+    validates :age, presence: true
     
     def password=(new_password)
         salt = BCrypt::Engine::generate_salt
