ca0de9c97078b4b07915996d0c9a68f09b318438
Samuel Amity <sam.amity@gmail.com>
2023-06-29 14:42:55 -0400

    build script correction

--- bin/vgreviews-build.sh
+++ bin/vgreviews-build.sh
@@ -1,8 +1,8 @@
 set -o errexit
 
 rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
+npm install --prefix frontend && npm run build --prefix frontend
+cp -a frontend/build/. public/
 
 bundle install
 bundle exec rake db:migrate
